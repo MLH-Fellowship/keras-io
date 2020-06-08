e05355f87410904349b350a5d13d0aea1ee0c41a
Joshua Newton <joshuacnewton@gmail.com>
2020-06-08 16:59:59 -0700

    Modify Keras imports to support tf.keras
    The old image_ocr.py example was written for Multi-Backend Keras. This
    commit provides compatibility for the latest tf.keras module.

--- examples/vision/image_ocr.py
+++ examples/vision/image_ocr.py
@@ -52,17 +52,18 @@ import editdistance
 import numpy as np
 from scipy import ndimage
 import pylab
-from keras import backend as K
-from keras.layers.convolutional import Conv2D, MaxPooling2D
-from keras.layers import Input, Dense, Activation
-from keras.layers import Reshape, Lambda
-from keras.layers.merge import add, concatenate
-from keras.models import Model
-from keras.layers.recurrent import GRU
-from keras.optimizers import SGD
-from keras.utils.data_utils import get_file
-from keras.preprocessing import image
-import keras.callbacks
+from tensorflow.keras import backend as K
+from tensorflow.keras.layers import Conv2D, MaxPooling2D
+from tensorflow.keras.layers import Input, Dense, Activation
+from tensorflow.keras.layers import Reshape, Lambda
+from tensorflow.keras.layers import add, concatenate
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import GRU
+from tensorflow.keras.optimizers import SGD
+from tensorflow.keras.utils import get_file
+from tensorflow.keras.preprocessing import image
+import tensorflow.keras.callbacks
+from tensorflow import keras
 
 
 OUTPUT_DIR = 'image_ocr'
