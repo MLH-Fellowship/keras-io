2ff3bf0605a87634852f55d2dd698cde56a4d820
Joshua Newton <joshuacwnewton@gmail.com>
2020-06-10 12:11:09 -0700

    Create skeleton for new OCR example

---
+++ examples/vision/image_ocr_new.py
@@ -0,0 +1,40 @@
+"""
+Title: End-to-end optical character recognition with CTC loss
+Author: [Joshua Newton](https://github.com/joshuacwnewton) & [Parthiv Chigurupati]()
+Date created: 2020/06/--
+Last modified: 2020/06/--
+Description: Optical character recognition of synthetic data using a recurrent network and CTC loss.
+"""
+"""
+## Introduction
+"""
+
+
+"""
+## Setup
+"""
+
+
+"""
+## Dataset
+"""
+
+
+"""
+## CTC Loss
+"""
+
+
+"""
+## Model preparation
+"""
+
+
+"""
+## Training routine
+"""
+
+
+"""
+## Sample inference on new data
+"""
