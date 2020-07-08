220bb22b6bc311390e11c503763e92acb57695a0
Kristi <kltakach@gmail.com>
2020-07-08 12:15:36 -0400

    made heatmap function to use matplotlib instead of seaborn.

--- examples/timeseries/timeseries_weather_forecasting.py
+++ examples/timeseries/timeseries_weather_forecasting.py
@@ -165,9 +165,18 @@ show_raw_visualization(df)
 """
 This heat map shows the correlation between different features.
 """
+def show_heatmap(data):
+    plt.matshow(data.corr())
+    plt.xticks(range(data.shape[1]), data.columns, fontsize=14, rotation=90)
+    plt.gca().xaxis.tick_bottom()
+    plt.yticks(range(data.shape[1]), data.columns, fontsize=14)
 
-corr = df.corr()
-sns.heatmap(corr)
+    cb = plt.colorbar()
+    cb.ax.tick_params(labelsize=14)
+    plt.title('Feature Correlation Heatmap', fontsize=14);
+    plt.show()
+
+show_heatmap(df)
 
 """
 The input data will include pressure, temperature (in Celsius) and specific humidity.
