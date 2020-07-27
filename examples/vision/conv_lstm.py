348a952c4e28cd43514dcab7b1972cef8662400e
Kristi <kltakach@gmail.com>
2020-07-27 17:48:12 -0400

    push for Cory to see

--- examples/vision/conv_lstm.py
+++ examples/vision/conv_lstm.py
@@ -26,54 +26,64 @@ We create a model which take as input movies of shape
 of identical shape.
 """
 
-""" seq = keras.Sequential(
-    [
-        keras.Input(
-            shape=(None, 40, 40, 1)
-        ),  # Variable-length sequence of 40x40x1 frames
-        layers.ConvLSTM2D(
-            filters=40, kernel_size=(3, 3), padding="same", return_sequences=True
-        ),
-        layers.BatchNormalization(),
-        layers.ConvLSTM2D(
-            filters=40, kernel_size=(3, 3), padding="same", return_sequences=True
-        ),
-        layers.BatchNormalization(),
-        layers.ConvLSTM2D(
-            filters=40, kernel_size=(3, 3), padding="same", return_sequences=True
-        ),
-        layers.BatchNormalization(),
-        layers.ConvLSTM2D(
-            filters=40, kernel_size=(3, 3), padding="same", return_sequences=True
-        ),
-        layers.BatchNormalization(),
-        layers.Conv3D(
-            filters=1, kernel_size=(3, 3, 3), activation="sigmoid", padding="same"
-        ),
-    ]
-) """
-
-
-#double check you need all the keras
+import os
+import pylab as plt
+import numpy as np
+import tensorflow as tf
+from keras.layers.normalization import BatchNormalization
+from keras.layers.convolutional_recurrent import ConvLSTM2D
+from keras.layers.convolutional import Conv3D
 from tensorflow import keras
 from tensorflow.keras import layers
 from keras import models
-from cv2 import cv2   #gets rid of pylint errors
-from keras.layers.convolutional import Conv3D
-from keras.layers.convolutional_recurrent import ConvLSTM2D
-from keras.layers.normalization import BatchNormalization
-import numpy as np
-import pylab as plt
-import os
-from cv2 import cv2
+from cv2 import cv2  # gets rid of pylint errors
+"""
+# editted input shape of the original code
+def create_model():
+    seq = keras.Sequential(
+        [
+            keras.Input(
+                shape=(None, 40, 40, 1 )
+            ),
+            # layers.ConvLSTM2D(
+            #     filters=60, kernel_size=(3, 3), padding="same", return_sequences=True
+            # ),
+            # layers.BatchNormalization(),
+
+            layers.ConvLSTM2D(
+                filters=40, kernel_size=(3, 3), padding="same", return_sequences=True
+            ),
+            layers.BatchNormalization(),
+            layers.ConvLSTM2D(
+                filters=40, kernel_size=(3, 3), padding="same", return_sequences=True
+            ),
+            layers.BatchNormalization(),
+            layers.ConvLSTM2D(
+                filters=40, kernel_size=(3, 3), padding="same", return_sequences=True
+            ),
+            layers.BatchNormalization(),
+            layers.ConvLSTM2D(
+                filters=40, kernel_size=(3, 3), padding="same", return_sequences=True
+            ),
+            layers.BatchNormalization(),
+            layers.Conv3D(
+                filters=1, kernel_size=(3, 3, 3), activation="sigmoid", padding="same"
+            ),
+        ]
+    )
+    seq.compile(loss="binary_crossentropy", optimizer="adadelta")
+
+    return seq
+"""
 def create_model():
     model = keras.Sequential()
+    # model = Sequential()
+
+    model.add(keras.Input(shape=(None, 40, 40, 1)))
+                         # 60 frames, 200x200 pi, 1 channel for black & white
 
     model.add(ConvLSTM2D(filters=40, kernel_size=(3, 3),
-                         # 60 frames, 200x200 pi, 1 channel for black & white
-                         input_shape=(60, 40, 40, 1),
                          padding='same', return_sequences=True))
-
     model.add(BatchNormalization())
 
     model.add(ConvLSTM2D(filters=40, kernel_size=(3, 3),
@@ -91,70 +101,13 @@ def create_model():
     model.add(Conv3D(filters=1, kernel_size=(3, 3, 3),
                      activation='sigmoid',
                      padding='same', data_format='channels_last'))
-
-    model.compile(loss='binary_crossentropy', optimizer='adadelta')
+    model.compile(loss="binary_crossentropy", optimizer="adadelta")
     return model
 
 
-"""
-def generate_movies(n_samples=1200, n_frames=15):
-    row = 80
-    col = 80
-    noisy_movies = np.zeros((n_samples, n_frames, row, col, 1), dtype=np.float)
-    shifted_movies = np.zeros((n_samples, n_frames, row, col, 1), dtype=np.float)
-
-    for i in range(n_samples):
-        # Add 3 to 7 moving squares
-        n = np.random.randint(3, 8)
-
-        for j in range(n):
-            # Initial position
-            xstart = np.random.randint(20, 60)
-            ystart = np.random.randint(20, 60)
-            # Direction of motion
-            directionx = np.random.randint(0, 3) - 1
-            directiony = np.random.randint(0, 3) - 1
-
-            # Size of the square
-            w = np.random.randint(2, 4)
-
-            for t in range(n_frames):
-                x_shift = xstart + directionx * t
-                y_shift = ystart + directiony * t
-                noisy_movies[
-                    i, t, x_shift - w : x_shift + w, y_shift - w : y_shift + w, 0
-                ] += 1
-
-                # Make it more robust by adding noise.
-                # The idea is that if during inference,
-                # the value of the pixel is not exactly one,
-                # we need to train the model to be robust and still
-                # consider it as a pixel belonging to a square.
-                if np.random.randint(0, 2):
-                    noise_f = (-1) ** np.random.randint(0, 2)
-                    noisy_movies[
-                        i,
-                        t,
-                        x_shift - w - 1 : x_shift + w + 1,
-                        y_shift - w - 1 : y_shift + w + 1,
-                        0,
-                    ] += (noise_f * 0.1)
-
-                # Shift the ground truth by 1
-                x_shift = xstart + directionx * (t + 1)
-                y_shift = ystart + directiony * (t + 1)
-                shifted_movies[
-                    i, t, x_shift - w : x_shift + w, y_shift - w : y_shift + w, 0
-                ] += 1
-
-    # Cut to a 40x40 window
-    noisy_movies = noisy_movies[::, ::, 20:60, 20:60, ::]
-    shifted_movies = shifted_movies[::, ::, 20:60, 20:60, ::]
-    noisy_movies[noisy_movies >= 1] = 1
-    shifted_movies[shifted_movies >= 1] = 1
-    return noisy_movies, shifted_movies
-"""
-
+# create_model()
+for layer in create_model().layers:
+    print(layer.output_shape)
 # frame generation
 
 # only have one sample
@@ -171,7 +124,6 @@ def generate_movies(n_frames=60):
     # use relative filepath to grab the frames
     # returns directory path of file
     dirname = os.path.dirname(__file__)
-
 
     # ================TESTING WITH COLOR WHEEL JPG =====================================================
     # filename = os.path.join(dirname + '/img/moving_square/color-wheel.jpg')
@@ -191,49 +143,55 @@ def generate_movies(n_frames=60):
     # # cv2.waitKey(0)
     # # cv2.destroyAllWindows()
 
-#====================ONCE TEST IMAGE IS WORKING, USE BELOW=========================================
+# ====================ONCE TEST IMAGE IS WORKING, USE BELOW=========================================
     counter = 0
 
     while (counter < n_frames):
-        filename = os.path.join(dirname + '/img/moving_square/frame_' + str(counter) + '.png')
-        
+        filename = os.path.join(
+            dirname + '/img/moving_square/frame_' + str(counter) + '.png')
+
         # 0 stands for grayscale
         img = cv2.imread(filename)
         grayImage = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        (thresh, blackAndWhiteImage) = cv2.threshold(grayImage, 0, 255, cv2.THRESH_BINARY)
-        #save B&W image
-        cv2.imwrite('./examples/vision/img/moving_square/bw_frame_'+ str(counter)+ '.png', blackAndWhiteImage)
+        (thresh, blackAndWhiteImage) = cv2.threshold(
+            grayImage, 0, 255, cv2.THRESH_BINARY)
+        # save B&W image
+        cv2.imwrite('./examples/vision/img/moving_square/bw_frame_' +
+                    str(counter) + '.png', blackAndWhiteImage)
         # blackAndWhiteImage.save('/img/moving_square/frameee_' + str(counter)+ '.png')
         counter += 1
 
     # display img
-    # cv2.imshow('image' + str(counter), img)
-
-    # cv2.imshow('grey image'+ str(counter), grayImage)
     # cv2.imshow('bw image'+ str(counter), blackAndWhiteImage)
-   
+
 #    # need this or window automatically closes
 #     cv2.waitKey(0)
 #     cv2.destroyAllWindows()
-
+    # Cut to a 40x40 window
+    noisy_movies = noisy_movies[::, 20:60, 20:60, ::]
+    shifted_movies = shifted_movies[::, 20:60, 20:60, ::]
+    noisy_movies[noisy_movies >= 1] = 1
+    shifted_movies[shifted_movies >= 1] = 1
+    return noisy_movies, shifted_movies
     # start training 100 copies without noise, if not perfect there's a problem. see if it memorizes.
 
 
 # test function call
-generate_movies()
+# generate_movies(n_frames)
 
 
 # planning to use the same training...
-"""
+
 # Train the model
 
 
-epochs = 1  # In practice, you would need hundreds of epochs.
-#need to figure out how to feed in the gif
-noisy_movies, shifted_movies = generate_movies(n_samples=1200)
-model.fit(
-    noisy_movies[:1000],   #features to predict on
-    shifted_movies[:1000],  #labels
+# epochs = 1  # In practice, you would need hundreds of epochs.
+epochs = 1
+# need to figure out how to feed in the gif
+noisy_movies, shifted_movies = generate_movies(n_frames=60)
+create_model().fit(
+    noisy_movies[:60],  # features to predict on
+    shifted_movies[:60],  # labels
     batch_size=10,
     epochs=epochs,
     verbose=2,
@@ -241,12 +199,12 @@ model.fit(
 )
 
 
-
 # Test the model on one movie
-
+"""
 Feed it with the first 7 positions and then
 predict the new positions.
-
+"""
+"""
 movie_index = 1004
 test_movie = noisy_movies[movie_index]
 
