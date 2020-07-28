4475bea5d1be02d1d1e968837df0fab2bd1e7d53
Kristi <kltakach@gmail.com>
2020-07-28 14:44:39 -0400

    it's making a prediction

--- examples/vision/conv_lstm.py
+++ examples/vision/conv_lstm.py
@@ -26,17 +26,6 @@ We create a model which take as input movies of shape
 of identical shape.
 """
 
-import os
-import pylab as plt
-import numpy as np
-import tensorflow as tf
-from keras.layers.normalization import BatchNormalization
-from keras.layers.convolutional_recurrent import ConvLSTM2D
-from keras.layers.convolutional import Conv3D
-from tensorflow import keras
-from tensorflow.keras import layers
-from keras import models
-from cv2 import cv2  # gets rid of pylint errors
 """
 # editted input shape of the original code
 def create_model():
@@ -74,16 +63,54 @@ def create_model():
     seq.compile(loss="binary_crossentropy", optimizer="adadelta")
 
     return seq
+
+    # ================TESTING WITH COLOR WHEEL JPG =====================================================
+    # filename = os.path.join(dirname + '/img/moving_square/color-wheel.jpg')
+    # # loads image from file
+    # img = cv2.imread(filename)
+    # grayImage = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # (thresh, blackAndWhiteImage) = cv2.threshold(grayImage, 249, 255, cv2.THRESH_BINARY)
+
+    # # test to see if values are 0 or 255
+    # # print (blackAndWhiteImage[10,33])
+    # # cv2.imshow('bw image', blackAndWhiteImage)
+    # path='./examples/vision/img/moving_square/bw-wheel2.jpg'
+    # cv2.imwrite(path, blackAndWhiteImage)
+
+    # #uncomment to see image
+    # # cv2.waitKey(0)
+    # # cv2.destroyAllWindows()
+
+
+
+
+
+
 """
+
+
+
+
+import os
+import pylab as plt
+import numpy as np
+import tensorflow as tf
+from keras.layers.normalization import BatchNormalization
+from keras.layers.convolutional_recurrent import ConvLSTM2D
+from keras.layers.convolutional import Conv3D
+from tensorflow import keras
+from tensorflow.keras import layers
+from keras import models
+from cv2 import cv2  # gets rid of pylint errors
 def create_model():
     model = keras.Sequential()
-    # model = Sequential()
 
+    # shape should be this
     model.add(keras.Input(shape=(None, 40, 40, 1)))
-                         # 60 frames, 200x200 pi, 1 channel for black & white
-
+    # 60 frames, 200x200 pi, 1 channel for black & white
     model.add(ConvLSTM2D(filters=40, kernel_size=(3, 3),
                          padding='same', return_sequences=True))
+
     model.add(BatchNormalization())
 
     model.add(ConvLSTM2D(filters=40, kernel_size=(3, 3),
@@ -106,92 +133,71 @@ def create_model():
 
 
 # create_model()
-for layer in create_model().layers:
-    print(layer.output_shape)
-# frame generation
+# trouble-shooting
+model1 = create_model()
+print(model1.name, model1.input_shape)
 
-# only have one sample
+for layer in model1.layers:
+    print(layer.name, layer.output_shape, layer.input_shape)
 
 
-def generate_movies(n_frames=60):
+def generate_movies(n_samples=10, n_frames=60):
     row = 80
     col = 80
-    # features
-    noisy_movies = np.zeros((n_frames, row, col, 1), dtype=np.float)
-    # keep this for labels
-    shifted_movies = np.zeros((n_frames, row, col, 1), dtype=np.float)
+    noisy_movies = np.zeros((n_samples, n_frames, row, col, 1), dtype=np.float) # features
+    shifted_movies = np.zeros((n_samples, n_frames, row, col, 1), dtype=np.float)  # labels
 
-    # use relative filepath to grab the frames
-    # returns directory path of file
     dirname = os.path.dirname(__file__)
 
-    # ================TESTING WITH COLOR WHEEL JPG =====================================================
-    # filename = os.path.join(dirname + '/img/moving_square/color-wheel.jpg')
-    # # loads image from file
-    # img = cv2.imread(filename)
-    # grayImage = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    # (thresh, blackAndWhiteImage) = cv2.threshold(grayImage, 249, 255, cv2.THRESH_BINARY)
-
-    # # test to see if values are 0 or 255
-    # # print (blackAndWhiteImage[10,33])
-    # # cv2.imshow('bw image', blackAndWhiteImage)
-
-    # path='./examples/vision/img/moving_square/bw-wheel2.jpg'
-    # cv2.imwrite(path, blackAndWhiteImage)
-
-    # #uncomment to see image
-    # # cv2.waitKey(0)
-    # # cv2.destroyAllWindows()
-
-# ====================ONCE TEST IMAGE IS WORKING, USE BELOW=========================================
     counter = 0
-
+    counter2=0
+    # wrap in for-loop going over samples=10; have a bunch of identical instances in the batch;
+    # make add noise func(after prediction works)
+    # while (counter2 in n_samples):
     while (counter < n_frames):
         filename = os.path.join(
             dirname + '/img/moving_square/frame_' + str(counter) + '.png')
 
-        # 0 stands for grayscale
         img = cv2.imread(filename)
         grayImage = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        (thresh, blackAndWhiteImage) = cv2.threshold(
-            grayImage, 0, 255, cv2.THRESH_BINARY)
-        # save B&W image
+        (thresh, blackAndWhiteImage) = cv2.threshold(grayImage, 0, 255, cv2.THRESH_BINARY)
+        #save B&W image
         cv2.imwrite('./examples/vision/img/moving_square/bw_frame_' +
                     str(counter) + '.png', blackAndWhiteImage)
-        # blackAndWhiteImage.save('/img/moving_square/frameee_' + str(counter)+ '.png')
         counter += 1
 
-    # display img
-    # cv2.imshow('bw image'+ str(counter), blackAndWhiteImage)
+        # #old testing
+        # #display img
+        # cv2.imshow('bw image'+ str(counter), blackAndWhiteImage)
+        # #need this or window automatically closes
+        # cv2.waitKey(0)
+        # cv2.destroyAllWindows()
 
-#    # need this or window automatically closes
-#     cv2.waitKey(0)
-#     cv2.destroyAllWindows()
-    # Cut to a 40x40 window
-    noisy_movies = noisy_movies[::, 20:60, 20:60, ::]
-    shifted_movies = shifted_movies[::, 20:60, 20:60, ::]
+        # Cut to a 40x40 window
+    noisy_movies = noisy_movies[::, ::, 20:60, 20:60, ::]
+    shifted_movies = shifted_movies[::, ::, 20:60, 20:60, ::]
     noisy_movies[noisy_movies >= 1] = 1
     shifted_movies[shifted_movies >= 1] = 1
     return noisy_movies, shifted_movies
-    # start training 100 copies without noise, if not perfect there's a problem. see if it memorizes.
+# start training 100 copies without noise, if not perfect there's a problem. see if it memorizes.
 
+# make identical batches, wrap in for loop,
 
 # test function call
 # generate_movies(n_frames)
 
 
-# planning to use the same training...
-
-# Train the model
-
-
+# ================= Train the model=========================================
 # epochs = 1  # In practice, you would need hundreds of epochs.
-epochs = 1
+epochs = 10
 # need to figure out how to feed in the gif
 noisy_movies, shifted_movies = generate_movies(n_frames=60)
-create_model().fit(
-    noisy_movies[:60],  # features to predict on
-    shifted_movies[:60],  # labels
+print(np.array(noisy_movies).shape)
+
+
+model1.fit(
+    noisy_movies,  # features to predict on
+    shifted_movies,  # labels
     batch_size=10,
     epochs=epochs,
     verbose=2,
